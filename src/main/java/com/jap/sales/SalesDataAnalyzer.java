c27a70e99e9a442033903bf7673d57a0010b32ff
Philip Meshach <philipmeshach.pm@gmail.com>
2022-09-09 17:12:27 +0530

    the file reader and buffer readers are implemented

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -1,6 +1,11 @@
 package com.jap.sales;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SalesDataAnalyzer {
@@ -8,6 +13,12 @@ public class SalesDataAnalyzer {
     public List<SalesRecord> readFile(String fileName) {
         List<SalesRecord> salesRecordList = new ArrayList<>();
         // read the CSV file
+        try {
+            FileReader fis = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fis);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         // get one row at a time
         // read the values from the row
         // use the values to create an object
@@ -23,6 +34,12 @@ public class SalesDataAnalyzer {
     ) {
         List<SalesRecord> salesRecordList = new ArrayList<>();
         // sort the sales record list, according to the criteria set by the amountComparator parameter
+        Collections.sort(salesRecordList, new Comparator<SalesRecord>() {
+            @Override
+            public int compare(SalesRecord o1, SalesRecord o2) {
+                return 0;
+            }
+        });
         return salesRecordList;
     }
 
@@ -33,6 +50,7 @@ public class SalesDataAnalyzer {
     ) {
         SalesRecord customerWhoSpentMaxTimeOnSite = new SalesRecord();
         // sort the sales data, according to the time spent on site, we want to find out the record
+
         // with the maximum time spent on site
         return customerWhoSpentMaxTimeOnSite;
     }
